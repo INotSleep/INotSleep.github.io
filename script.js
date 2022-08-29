db2b3b0a5741d10e556bce31348bd6b27f1139e1
INotSleep <77581131+INotSleep@users.noreply.github.com>
2022-08-29 13:11:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ class Util {
 
 function updateBalance() {
 	var inrow = document.getElementsByClassName("inrow")
-	//Storage.get()
+	Storage.get()
 	var balance = Storage.data.balance
 	moneyE = []
 	for (var i = 0; i< inrow.length; i++){
@@ -138,7 +138,7 @@ function updateBalance() {
 
 function updateLevel() {
 	var inrow = document.getElementsByClassName("inrow")
-	//Storage.get()
+	Storage.get()
 	expE = []
 	for (var i = 0; i< inrow.length; i++){
 		if (inrow[i].id == "exp") expE.push(inrow[i])
@@ -266,7 +266,6 @@ function init() {
 
 if (document.getElementsByClassName("container-lg").length == 1) {
 document.getElementsByTagName("html")[0].innerHTML=`<html><head><title>RPG Game</title></head><body><link rel="stylesheet" href="./style.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><script src="./script.js"></script><div class="row" id="row"><a id="money" class="inrow"><img width="12" height="12" src="./images/coin.png"> 0</a><a id="exp" value="10" max="100" class="inrow"><img width="12" height="12" src="./images/star.png"> 0<progress id="expb" value="0" max="0" style="margin-left: 5px"></progress></a><button class="inrow" id="shopButton" onClick="toggleShop()">Магазин</button><div class="inrow" style="max-width: 182; position: absolute; right: 2px; top: 8px"><a>Hosted using <a href="https://pages.github.com/" style="color: white">Github pages</a></a></div></div><div class="window" id="game"><div class="inv"><a>Инвентарь</a><div class="invi" id="invi"></div></div><div class="gamec" id="gamec" style="max-width: 755px; height: 581px;"><button class="button" onclick="attack()">В бой</button><button class="button" onclick="upg()">Прокачки</button><div class="profile" id="profile"><a id="money" class="inrow" style="margin-left: unset;position: unset;top: unset;"><img width="12" height="12" src="./images/coin.png"> 0</a><div style="max-height: 11;"><a><br></a></div><a id="exp" value="10" max="100" class="inrow" style="margin-left: unset;position: unset;top: unset;"><img width="12" height="12" src="./images/star.png"> 0<progress id="expb" value="0" max="0" style="margin-left: 5px"></progress></a></div></div></div><div class="window" id="shop"></div></body></html>`
-setTimeout(()=>{init()},200)
 }
 setTimeout(init, 100)
 setInterval(updateGame, 100)
